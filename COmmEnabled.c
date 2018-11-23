5209a40f4b52bc148ab3355a45fca5b360add2a5
JohnWHaney <42155045+JohnWHaney@users.noreply.github.com>
2018-11-22 23:15:35 -0500

    Create COmmEnabled.c

---
+++ COmmEnabled.c
@@ -0,0 +1,14 @@
+/**************
+ *** this is code that does not work.
+ *** just for demo.  
+ ***/
+/*****************
+ *** Method three is the one you want to use
+ ***/
+ int m_iReturnCommEnabled(void)
+ {
+  int iRet;
+  iRet=3;
+  return (iRet);
+  }
+ 
