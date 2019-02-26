cd254ad4998a8114bffb0b60004ba486a7f92c87
JohnWHaney <42155045+JohnWHaney@users.noreply.github.com>
2019-02-26 15:27:19 -0500

    Update SDSourceCode.c
    as described by requirement FCSG-399 
    added string to define Self Drive ECU ID

--- SDSourceCode.c
+++ SDSourceCode.c
@@ -4,7 +4,9 @@
 
 #include <stdio.h>
 int main()
-{
+{ 
+   char *cEcuSdId;   // ECU ID for Self Drive
+   
    // printf() displays the string inside quotation
    printf("Hello, World!");
    return 0;
