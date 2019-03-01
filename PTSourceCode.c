523ebbb823b24a62745eaa7e5d2a7b2b80ad9ca0
JohnWHaney <42155045+JohnWHaney@users.noreply.github.com>
2019-03-01 11:55:53 -0500

    Update PTSourceCode.c
    Added Boolean to identify if is Power Train ECU ID for FCSG-402 requirement

--- PTSourceCode.c
+++ PTSourceCode.c
@@ -1,5 +1,14 @@
 /*
- *  SDSourceCode.c
+ *  PTSourceCode.c
  */ 
 
-
+#include <stdio.h>
+int main()
+{ 
+   bool bIsPowerTrainECU;    // ECU ID for Self Drive
+   
+  bIsPowerTrainECU = false;
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
+}
