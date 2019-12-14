b086960228cdcf6ad1a1874620c91ec47ca317db
JohnWHaney <42155045+JohnWHaney@users.noreply.github.com>
2019-12-13 19:12:06 -0500

    Update SDSourceCode.c
    Change in github.com and did not push or pull from GitHub desktop.  check TPR-293 to see if linked revision is added.   In fact github desktop was shut down.

--- SDSourceCode.c
+++ SDSourceCode.c
@@ -3,7 +3,7 @@
 //
 
 // adding change Polarion
-
+// testing GitHub desktop connection when making changes from github.com
 
 #include <stdio.h>
 int main()
@@ -11,7 +11,7 @@ int main()
    char *cEcuSdId;   // ECU ID for Self Drive
    
    int iFlag;  // added for demo 
-   
+   int ib; 
    // printf() displays the string inside quotation
    printf("Hello, World!");
    return 0;
